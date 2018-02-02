22de479f4e460a7de30d3a5aba408a173057c15c
odCat <gus666xe@gmail.com>
2018-02-02 05:32:01 +0200

    Update class names, add condition to deleteGrid(), remove whitespaces at end of lines, refactor css code

--- designs.js
+++ designs.js
@@ -1,24 +1,26 @@
 // Initialize background color
 const BGCOLOR="#ffffff"
 // Initialize the drawing color
-let fgColor=$(".colorPicker").val();
+let fgColor=$(".color-picker").val();
+// Initialize grid flag
+let isGrid=Boolean("falst");
 
 /*
  * Function to create the drawing grid
  */
 function makeGrid(heigth, width) {
-	console.log("Log: makeGrid(): " + heigth + " " + width);	
+	console.log("Log: makeGrid(): " + heigth + " " + width);
 	deleteGrid();
 	for (var i = 0; i < heigth; i++)
 	{
-		$(".pixelCanvas").append("<tr class=\"gridRow\"></tr>");
+		$(".pixel-canvas").append("<tr class=\"gridRow\"></tr>");
 		console.log("Log: makeGrid(): Rows" + i);
 	}
 	for (i = 0; i < width; i++)
 	{
 		$(".gridRow").each(function ()
 			{
-				$(this).append("<td class=\"gridCell\"></td>");
+				$(this).append("<td class=\"grid-cell\"></td>");
 			});
 		console.log("Log: makeGrid(): Cells" + i);
 	}
@@ -36,12 +38,22 @@ function deleteGrid()
 /*
  * Event on submit button click - choose size and draw grid
  */
-$(".drawGrid").click(function ()
+$(".draw-grid-button").click(function ()
 	{
 		let height = $(".input-height").val();
 		let width = $(".input-width").val();
+		
+		// Reset input text boxes values
+		$(".input-height").val("");
+		$(".input-width").val("");
+
 		console.log("height = " + height + "; width = " + width);
+
+		// Delete grid if grid exists
+		if (isGrid)
+		{
 		deleteGrid();
+		}
 
 		/* TODO: add erroneous input handling */
 		if (height && width)
@@ -54,15 +66,15 @@ $(".drawGrid").click(function ()
 /*
  * Event on color change 
  */
-$(".colorPicker").on("change", function ()
+$(".color-picker").on("change", function ()
 	{
-		fgColor = $(".colorPicker").val();
+		fgColor = $(".color-picker").val();
 	});
 
 /*
  * Event on table cell click - change background color to selected color
  */
-$(".pixelCanvas").on("click",".gridCell", function ()
+$(".pixel-canvas").on("click",".grid-cell", function ()
 	{
 		console.log("td: on click event");
 		$(this).css("background-color",fgColor);
